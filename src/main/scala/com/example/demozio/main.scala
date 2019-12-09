ec63f3d6e4cd44ca51049f6ac994965e69d36bda
octav <zahariao@gmail.com>
2019-12-09 13:32:43 +0200

    Integrate transactor with the rest

--- src/main/scala/com/example/demozio/main.scala
+++ src/main/scala/com/example/demozio/main.scala
@@ -2,67 +2,68 @@ package com.example.demozio
 
 import zio._
 import zio.console._
+import zio.blocking.Blocking
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Directives._
-
 import akka.stream.ActorMaterializer
-
 import modules._
+
 import models._
 import configuration._
 import repository._
+import transactor._
 
-object main  extends App {
-
-  private val env = new SettingsLive with Repository with Console.Live {
-    override val repository: Repository.Service = new RepositoryServiceLive {
-      override val xa: doobie.Transactor[Task] = ???
-    }
-  }
-
+object main extends App {
   import io.circe.generic.auto._
   import de.heikoseeberger.akkahttpcirce._
   import FailFastCirceSupport._
 
-  def route = get {
-    path("user") {
-      val json = """{"email": "test@example.com", "password": "12345"}"""
-      complete(HttpEntity(ContentTypes.`application/json`, json))
+  private def env(t: doobie.Transactor[Task]) = new SettingsLive with Repository with Console.Live {
+    override val repository: Repository.Service = new RepositoryServiceLive {
+      override val xa: doobie.Transactor[Task] = t
+    }
+  }
+
+  def route(xa: doobie.Transactor[Task]) = get {
+    path("user" / Segment) { email =>
+      val zio = getUserByEmail(email).provide(env(xa))
+      complete(unsafeRunToFuture(zio))
     }
   } ~
-   post {
-     path("user") {
-       entity(Directives.as[User]) { user =>
-         val zio = saveUser(user).provide(env)
-         complete(unsafeRunToFuture(zio))
-       }
-     }
-   }
+    post {
+      path("user") {
+        entity(Directives.as[User]) { user =>
+          val zio = saveUser(user).provide(env(xa))
+          complete(unsafeRunToFuture(zio))
+        }
+      }
+    }
 
   def akkaApp = for {
     config <- appConfig
     implicit0(as: ActorSystem) <- actorSystem
     implicit0(am: ActorMaterializer) <- ZIO(ActorMaterializer())
-    listener <- ZIO.fromFuture(_ => Http().bindAndHandle(route, "0.0.0.0", config.httpPort))
+    xa <- ZIO.access[Repository](_.repository.xa)
+    listener <- ZIO.fromFuture(_ => Http().bindAndHandle(route(xa), "0.0.0.0", config.httpPort))
   } yield listener
 
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
-   val zio = for {
-     config <- appConfig
-     _ <- migration.migrate(
-       config.database.schema,
-       config.database.jdbcUrl,
-       config.database.user,
-       config.database.password)
-     _ <- akkaApp *> ZIO.never
-    } yield ()
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
+    newTransactor.provide(new SettingsLive with Blocking.Live {}).orDie.use { xa =>
+      val zio = for {
+        config <- appConfig
+        _ <- migration.migrate(
+          config.database.schema,
+          config.database.jdbcUrl,
+          config.database.user,
+          config.database.password)
+        _ <- akkaApp *> ZIO.never
+      } yield ()
 
-    zio.provide(env).foldM(handleError, _ => ZIO.succeed(0))
-  }
+      zio.provide(env(xa)).foldM(handleError, _ => ZIO.succeed(0))
+    }
 
   def handleError(e: Throwable): UIO[Int] =
     ZIO(e.printStackTrace()).as(1).orDie
--- src/main/scala/com/example/demozio/modules/Repository.scala
+++ src/main/scala/com/example/demozio/modules/Repository.scala
@@ -8,10 +8,17 @@ import doobie._
 import doobie.implicits._
 
 sealed trait DatabaseError extends  Throwable
-final case class GenericDatabaseError(inner: Throwable) extends DatabaseError
+final case class GenericDatabaseError(inner: Throwable) extends DatabaseError {
+  override def getMessage = inner.getMessage
+
+  override def getStackTrace: Array[StackTraceElement] = inner.getStackTrace
+
+}
 
 object Repository {
   trait Service {
+    val xa: Transactor[Task]
+
     def saveUser(user: User): IO[DatabaseError, User]
     def getUserByEmail(email: String): IO[DatabaseError, Option[User]]
   }
@@ -22,8 +29,6 @@ trait Repository {
 }
 
 trait RepositoryServiceLive extends Repository.Service {
-  val xa: Transactor[Task]
-
   override def saveUser(user: User) = {
     val sqlInsert =    sql"""insert into users(email, password) values(${user.email}, ${user.password})""".update.run
     sqlInsert.transact(xa).mapError(GenericDatabaseError).as(user)
