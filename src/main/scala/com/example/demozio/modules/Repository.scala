2f767f9991d98c906f73b91e8e72baef06bd7ef7
octav <zahariao@gmail.com>
2019-12-09 10:36:34 +0200

    Implement repository

--- src/main/scala/com/example/demozio/modules/Repository.scala
+++ src/main/scala/com/example/demozio/modules/Repository.scala
@@ -2,12 +2,18 @@ package com.example.demozio
 package modules
 
 import zio._
+import zio.interop.catz._
 import models._
+import doobie._
+import doobie.implicits._
+
+sealed trait DatabaseError extends  Throwable
+final case class GenericDatabaseError(inner: Throwable) extends DatabaseError
 
 object Repository {
   trait Service {
-    def saveUser(user: User): Task[User]
-    def getUserByEmail(email: String): Task[User]
+    def saveUser(user: User): IO[DatabaseError, User]
+    def getUserByEmail(email: String): IO[DatabaseError, Option[User]]
   }
 }
 
@@ -15,8 +21,16 @@ trait Repository {
   val repository: Repository.Service
 }
 
-//we need a database
 trait RepositoryServiceLive extends Repository.Service {
-  override def saveUser(user: User): Task[User] = ???
-  override def getUserByEmail(email: String): Task[User] = ???
+  val xa: Transactor[Task]
+
+  override def saveUser(user: User) = {
+    val sqlInsert =    sql"""insert into users(email, password) values(${user.email}, ${user.password})""".update.run
+    sqlInsert.transact(xa).mapError(GenericDatabaseError).as(user)
+  }
+
+  override def getUserByEmail(email: String) = {
+    val sqlGetByEmail = sql"""select email, password from users where email=$email""".query[User].option
+    sqlGetByEmail.transact(xa).mapError(GenericDatabaseError)
+  }
 }
